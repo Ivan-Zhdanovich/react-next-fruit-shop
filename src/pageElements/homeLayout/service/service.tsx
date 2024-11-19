43de3e24b4880b7e3deabac6e83b54b6fa8f4ce6
Zhdanovich <Zhiv80@mail.ru>
2024-11-19 14:14:32 +0300

    Fix: change styles

--- src/pageElements/homeLayout/service/service.tsx
+++ src/pageElements/homeLayout/service/service.tsx
@@ -1,24 +1,26 @@
 import Image from "next/image";
 import basket from "../../../assets/pages/mainPage/basket.png";
 
-import style from "./service.module.scss";
+import styles from "./service.module.scss";
 
 export default function Service() {
   return (
-    <div className={style.mainPageContainer}>
-    <div className={style.mainPageContainer__Header}>
-    <h1>Наслаждайся покупками, не экономь на себе.</h1>
-      <p className={style.mainPageContainer__Header__Text}>
+    <div className={styles.mainPageContainer}>
+    <div className={styles.mainPageContainer__Header}>
+    <h1 className={styles.mainPageContainer__Header__Title}>Наслаждайся покупками, не экономь на себе.</h1>
+      <p className={styles.mainPageContainer__Header__Text}>
         Online магазин - самые лучшие покупки здесь. Нас легко найти и невозможно
         забыть. Давайте получим удовольствие вместе от ваших покупок. Приятного
         выбора. Ждем Вас снова у нас. До скорой встречи.
       </p>
     </div>
-     <Image
-        className={style.mainPicture}
+    <div className={styles.mainPictureWrapper}>
+    <Image
+        className={styles.mainPicture}
         src={basket}
         alt="корзина для покупок"
       />
+    </div>
     </div>
   );
 }
